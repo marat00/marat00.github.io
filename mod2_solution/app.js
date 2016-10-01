84ffce148cbb5b6d2ed1e1b402daf8951d0c5a8a
Mark P <marat75@gmail.com>
2016-09-30 20:58:51 -0700

    Removed a link

--- mod2_solution/app.js
+++ mod2_solution/app.js
@@ -1,46 +1,60 @@
 (function () {
 'use strict';
   
-angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController);
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
   
-LunchCheckController.$inject = ['$scope'];
-function LunchCheckController($scope) {
-    $scope.response = ""; //default message
-	$scope.lunch = ""; // to hold the user input for lunch entries
-	  
-    $scope.determineResponse = function () {
-	    var lunch = $scope.lunch.split(',').filter(isValidEntry); // using Array.prototype.filter
-		
-		// We need to check if the number of elements is greater than 1
-		if (lunch.length > 0) {
-		  $scope.response = responseChecker(lunch);
-        } else {
- 		  $scope.response = "Please enter data first";
-		}
-	};
-	
-	// The function to check is the input is a String (might be redundant) and
-	// if its length is greater than zero
-	// This should filter empty strings, such as "" and "  ".
-	function isValidEntry(entry) {
-		return (Object.prototype.toString.call(entry) === '[object String]') && (entry.length > 1);
-	}
-	
-	// Check if the number of elements is less than or equal to 3, or greater than 3.
-	// If the number of elements is less than or equal to 3, the method returns "Enjoy!".
-	// If the number of elements is greater than 3, return "Too much!".
-	function responseChecker(lunchItems) {
-		var response = "";
-		
-		if (lunchItems.length <= 3) {
-			response = "Enjoy!";
-		} else if (lunchItems.length > 3){
-			response = "Too much!";
-	    } else
-			response = "Please enter data first";
-		
-		return response;
-	}
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var itemAdder = this;
+
+  itemAdder.itemName = "";
+  itemAdder.itemQuantity = "";
+
+  itemAdder.addItem = function () {
+    ShoppingListCheckOffService.addItem(itemAdder.itemName, itemAdder.itemQuantity);
+  }
 }
+
+//Controller for the already bought items
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var boughtList = this;
+
+  boughtList.items = ShoppingListCheckOffService.getItems();
+
+  boughtList.removeItem = function (itemIndex) {
+    ShoppingListCheckOffService.removeItem(itemIndex);
+  };
+}
+
+
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  // List of shopping items
+  var items = [
+     "Cookie", "Waffle", "Candy", "Chocolate", "Pudding", "Donut"
+  ];
+
+  service.addItem = function (itemName, quantity) {
+    var item = {
+      name: itemName,
+      quantity: quantity
+    };
+	
+    items.push(item);
+  };
+
+  service.removeItem = function (itemIndex) {
+    items.splice(itemIndex, 1);
+  };
+
+  service.getItems = function () {
+    return items;
+  };
+}
+
 })();
